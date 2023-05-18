9c05ec0e0c3a6d5d53f3cbdada2298fb28bff86d
William Lowe <walowe@deakin.edu.au>
2023-05-18 21:05:26 +1000

    docs:SubmissionEnhancementFiles
    Associated Submission Enhancement Files
    
    Will

---
+++ docs/OnTrack/Task Submission & Redesign/SubmissionEnhancement/backend/app.js
@@ -0,0 +1,41 @@
+
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+const port = 4000;
+const app = express();
+
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+var upload = multer({ storage: storage });
+
+app.post("/file", upload.single("file"), function (req, res, next) {
+  const file = req.file;
+  if (file) {
+    res.json(req.file);
+  } else throw "error";
+});
+
+app.post("/multiplefiles", upload.array("files"), function (req, res, next) {
+  const files = req.files;
+  if (Array.isArray(files) && files.length > 0) {
+    res.json(req.files);
+  } else {
+    res.status(400);
+    throw new Error("No file");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost: ` + port);
+});
---
+++ docs/OnTrack/Task Submission & Redesign/SubmissionEnhancement/frontend/src/app/components/test-diff/test-diff.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import * as DiffMatchPatch from 'diff-match-patch';
+import { diff_match_patch } from 'diff-match-patch';
+
+@Component({
+  selector: 'app-test-diff',
+  templateUrl: './test-diff.component.html',
+  styleUrls: ['./test-diff.component.css']
+})
+export class TestDiffComponent {
+  text1 = '';
+  text2 = '';
+  diff: string | undefined;
+
+  computeDiff() {
+    const dmp = new DiffMatchPatch.diff_match_patch();
+    const diffs = dmp.diff_main(this.text1, this.text2);
+    dmp.diff_cleanupSemantic(diffs);
+  
+    this.diff = dmp.diff_prettyHtml(diffs)
+      .replace(/&lt;ins&gt;(.*?)&lt;\/ins&gt;/g, '<span class="diff-ins"></span>')
+      .replace(/&lt;del&gt;(.*?)&lt;\/del&gt;/g, '<span class="diff-del"></span>');
+  }
+}
+
